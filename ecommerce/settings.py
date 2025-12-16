ecd20deceab9f85b0bb4504d66f3ffd4677902ed
SuriyaPrakash <sksuriyakannan@gmail.com>
2025-12-16 17:43:56 +0530

    update debug true

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["m2hit.in", "www.m2hit.in",'13.204.192.59','*' ,"127.0.0.1", "localhost"]
 
